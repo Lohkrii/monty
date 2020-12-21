46d02e3e3c33d2aaa50d481629a9852dd8782beb
GorillaFu <jared@beguelin.com>
2020-12-21 17:26:09 +0000

    monty header file

---
+++ monty.h
@@ -0,0 +1,12 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdlib.h>
+
+/* stack functions */
+void push(stack_t **head, unsigned int line, char *n)
+void pop(stack_t **head, unsigned int line)
+void swap(stack_t **head, unsigned int line)
+void nop(stack_t **head, unsigned int line)
+
+#endif
