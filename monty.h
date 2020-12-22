841a83a202190d348dfae36f6d278fc4f7b11364
GorillaFu <jared@beguelin.com>
2020-12-22 22:28:09 +0000

    added calc functions

--- monty.h
+++ monty.h
@@ -13,4 +13,11 @@ void pop(stack_t **head, unsigned int line);
 void swap(stack_t **head, unsigned int line);
 void nop(stack_t **head, unsigned int line);
 
+/* math functions */
+void add(stack_t **head, unsigned int line_num);
+void sub(stack_t **head, unsigned int line_num);
+void mul(stack_t **head, unsigned int line_num);
+void div_stack(stack_t **head, unsigned int line_num);
+void mod(stack_t **head, unsigned int line_num);
+
 #endif /* MONTY_H */
